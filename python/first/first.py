fcbd33e1610a4b6a75abf7b5222a094ef97e6d3f
sandzhr0123 <sandzhr0123@gmail.com>
2018-04-21 14:46:22 +0800

    增加python

---
+++ python/first/first.py
@@ -0,0 +1,17 @@
+﻿# encoding: utf-8  
+print("1111");
+def a():
+    print("a");
+    if True:
+        print(1);
+    else:
+        print(2);
+        print(u"执行完了");
+
+
+import json
+
+data = [ { 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5 } ]
+
+json = json.dumps(data)
+print data;
